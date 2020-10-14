4dc16c2104dc5173037b5225eb0e42cc4b391866
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-10-14 18:06:58 +0200

    Eliminar constantes

--- vista-compradores.php
+++ vista-compradores.php
@@ -1,32 +1,23 @@
 <?php
-
 include "./templates/header.php";
 include "./classes/class.db.php";
-
 $mostrarCompradores = new DBforms();
-
-const OBJETO = "compradores";
-const MENU = "index.php";
-
 // Muestro la vista en HTML
 ?>
-
 <div class="caja-contenedora">
     <div class="caja-seleccion">
-        <a class="button" href="./"<?php MENU ?> >Menú</a>
+        <a class="button" href="./index.php">Menú</a>
     </div>
     <h3>
-        Mostrar <?php echo OBJETO ?>
+        Mostrar compradores
     </h3>
-
-    <hr> 
-    
+    <hr>
     <?php 
     $listaCompradores = $mostrarCompradores->obtenerCompradores();
     echo "<p>";
     while ($valores = mysqli_fetch_array($query)) {
         // En esta sección estamos llenando el select con datos extraidos de una base de datos.
-        echo $valores['idComprador'].' '.$valores['idPersona']; // .' '.$valores['nombre'].' '.$valores['apellidos'].' '.$valores['dni'];
+        echo $valores['idComprador'] . ' ' . $valores['idPersona']; // .' '.$valores['nombre'].' '.$valores['apellidos'].' '.$valores['dni'];
     }
     echo "</p>";
     ?>
